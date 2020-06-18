1da91a21d5106c4fb4c4105506a74bd4301833e9
ammar <ammmmmaeh@gmail.com>
2020-06-18 10:14:01 +0200

    Signed-off-by: ammar <ammmmmaeh@gmail.com>

---
+++ app/src/main/java/com/example/hanaiabeauty/catograyprodect/show/showstore/Store3Activity.java
@@ -0,0 +1,14 @@
+package com.example.hanaiabeauty.catograyprodect.show.showstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Store3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store3);
+    }
+}
